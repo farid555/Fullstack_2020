4ceab42f81cfc21444397e0b636d5341e6929b21
farid555 <mohammad.uzzaman2@centria.fi>
2020-09-25 11:33:28 +0300

    Exercise 2.2

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -17,6 +17,7 @@ const Content = (props) => {
       <Part1 part_1={props.course.parts[0].name} exer_cises1={props.course.parts[0].exercises} />
       <Part2 part_2={props.course.parts[1].name} exer_cises2={props.course.parts[1].exercises} />
       <Part3 part_3={props.course.parts[2].name} exer_cises3={props.course.parts[2].exercises} />
+      <Part4 part_4={props.course.parts[3].name} exer_cises4={props.course.parts[3].exercises} />
     </div>
   )
 }
@@ -44,13 +45,21 @@ const Part3 = (props) => {
     </div>
   )
 }
+const Part4 = (props) => {
+
+  return (
+    <div>
+      <p>{props.part_4} {props.exer_cises4} </p>
+    </div>
+  )
+}
 
 const Total = (props) => {
   console.log(props)
   return (
     <div>
-      <p>Number of exercises {props.course.parts[0].exercises +
-        props.course.parts[1].exercises + props.course.parts[2].exercises} </p>
+      <p><b>Number of exercises {props.course.parts[0].exercises +
+        props.course.parts[1].exercises + props.course.parts[2].exercises + props.course.parts[3].exercises} </b></p>
     </div>
   )
 }
@@ -62,6 +71,7 @@ const Course = ({ course }) => {
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
@@ -85,6 +95,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
