7235cb3e2b5ff79ec583f786521846488d92c970
farid555 <mohammad.uzzaman2@centria.fi>
2020-11-05 11:12:20 +0200

    Exercise 4.5 done

--- part4/Blog/utils/list_helper.js
+++ part4/Blog/utils/list_helper.js
@@ -1,14 +1,27 @@
 const dummy = (blogs) => {
   // ...
-  return 1;
+  return 1
 };
 const totalLikes = (blogs) => {
   const reducer = (sum, blog) => {
     return sum + blog.likes;
   };
-  return blogs.reduce(reducer, 0);
+  return blogs.reduce(reducer, 0)
+};
+const favoriteBlog = (blogs) => {
+  let fav = blogs[0];
+  blogs.forEach((blog) => {
+    if (blog.likes > fav.likes) {
+      fav = blog;
+    }
+  })
+  delete fav._id
+  delete fav.__v
+  delete fav.url
+  return fav
 };
 module.exports = {
   dummy,
   totalLikes,
-};
+  favoriteBlog,
+}
