c81338bd81d114de07ca6adf13d8bb0c48831295
Mohammad Farid Uzzaman SOFTK20P <mohammad.uzzaman2@centria.fi>
2020-11-11 12:20:39 +0200

    Exercise 4.19 & 4.20 is Done!

--- part4/Blog/controllers/blogs.js
+++ part4/Blog/controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 
 
 blogsRouter.get('/', async (request, response) => {
@@ -25,15 +35,19 @@ blogsRouter.get('/:id', (request, response, next) => {
 
 blogsRouter.post("/", async (request, response) => {
   const body = request.body
-  const users = await User.find({})
-  const user = await User.findById(users[0]._id)
+  const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-    user: users[0]._id
+    user: user._id
   })
   
 const savedBlog = await blog.save()
